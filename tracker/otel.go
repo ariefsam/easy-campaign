26b3621f7b930dd305966e08cf9ee0094a0b7318
Arief Hidayatulloh <ariefhidayatulloh@gmail.com>
2025-03-09 09:24:21 +0700

    environment variable for sentry

--- tracker/otel.go
+++ tracker/otel.go
@@ -10,12 +10,21 @@ import (
 
 func Init() {
 	dsn := os.Getenv("SENTRY_DSN")
+	environment := os.Getenv("ENVIRONMENT")
+	if dsn == "" {
+		log.Println("SENTRY_DSN is empty")
+		return
+	}
+	if environment == "" {
+		environment = "not-set"
+	}
 	err := sentry.Init(sentry.ClientOptions{
 		Dsn: dsn,
 		// Set TracesSampleRate to 1.0 to capture 100%
 		// of transactions for tracing.
 		// We recommend adjusting this value in production,
 		TracesSampleRate: 1.0,
+		Environment:      environment,
 	})
 	if err != nil {
 		log.Println("sentry.Init: %s", err)
